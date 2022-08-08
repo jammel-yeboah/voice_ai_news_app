2fe7326bc9c9ad6af854d9f8808adb5cc7178ff6
Jammel <jammelyeboah@gmail.com>
2022-08-08 21:40:01 +0100

    Updates

---
+++ precache-manifest.870cf95c303ae03176523b7f44e58b9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83065666beb355ef093f1e79ea297816",
+    "url": "/voice_ai_news_app/index.html"
+  },
+  {
+    "revision": "fd2cd89e1cd5e81e7b24",
+    "url": "/voice_ai_news_app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5c8c07faa31e436014d3",
+    "url": "/voice_ai_news_app/static/css/main.8fd67772.chunk.css"
+  },
+  {
+    "revision": "fd2cd89e1cd5e81e7b24",
+    "url": "/voice_ai_news_app/static/js/2.a1d7d8b7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/voice_ai_news_app/static/js/2.a1d7d8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8c07faa31e436014d3",
+    "url": "/voice_ai_news_app/static/js/main.349cd355.chunk.js"
+  },
+  {
+    "revision": "869c32859ce43290b96f",
+    "url": "/voice_ai_news_app/static/js/runtime-main.5f22bc76.js"
+  },
+  {
+    "revision": "289eea3824e6d82a4b43af5f0380a904",
+    "url": "/voice_ai_news_app/static/media/logo.289eea38.svg"
+  }
+]);
